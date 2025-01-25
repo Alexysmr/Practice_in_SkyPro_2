60be5880066816d2e4751cb5797d5567c17c5666
Fomenko Aleksey <alexxsmr@yandex.ru>
2025-01-25 19:58:58 +0400

    Modified:     test\test_class_category.py

--- tests/test_class_category.py
+++ tests/test_class_category.py
@@ -5,8 +5,8 @@ def test_category_init1(product_category1, product_category2):
                                             "дополнительных функций для удобства жизни")
     assert len(product_category1.products) == 2
     assert product_category2.name == "Телевизоры"
-    assert product_category2.description == ("Современный телевизор, который позволяет наслаждаться просмотром, станет "
-                                             "вашим другом и помощником")
+    assert product_category2.description == ("Современный телевизор, который позволяет наслаждаться просмотром, "
+                                             "станет вашим другом и помощником")
     assert len(product_category2.products) == 1
     assert product_category2.category_count == 2
     assert product_category2.product_count == 3
