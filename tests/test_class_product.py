da2907baff1c0e853af0be0330b49d1fbd63e90d
Fomenko Aleksey <alexxsmr@yandex.ru>
2025-01-26 08:30:39 +0400

    Modified:     test\test_class_category.py          conftest.py          test_class_product.py          test_utils.py

--- tests/test_class_product.py
+++ tests/test_class_product.py
@@ -1,4 +1,7 @@
-
 def test_product_init(device1):
     assert (device1.name, device1.description, device1.price, device1.quantity) == (
-    "Realme 13+", "256GB, Розовый цвет, 46MP камера", 16000.0, 5)
+        "Realme 13+",
+        "256GB, Розовый цвет, 46MP камера",
+        16000.0,
+        5,
+    )
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,34 @@
-
-from src.utils import read_json, make_from_json
+from src.utils import make_from_json, read_json
 
 path = "data/products.json"
 
 
 def test_read_json():
-    assert read_json(path) == [{
-                                   'description': 'Смартфоны, как средство не только коммуникации, но и получение '
-                                                  'дополнительных функций для удобства жизни',
-                                   'name': 'Смартфоны', 'products': [
-            {'description': '256GB, Серый цвет, 200MP камера', 'name': 'Samsung Galaxy C23 Ultra', 'price': 180000.0,
-             'quantity': 5},
-            {'description': '512GB, Gray space', 'name': 'Iphone 15', 'price': 210000.0, 'quantity': 8},
-            {'description': '1024GB, Синий', 'name': 'Xiaomi Redmi Note 11', 'price': 31000.0, 'quantity': 14}]}, {
-                                   'description': 'Современный телевизор, который позволяет наслаждаться просмотром, '
-                                                  'станет вашим другом и помощником',
-                                   'name': 'Телевизоры', 'products': [
-            {'description': 'Фоновая подсветка', 'name': '55" QLED 4K', 'price': 123000.0, 'quantity': 7}]}]
+    assert read_json(path) == [
+        {
+            "description": "Смартфоны, как средство не только коммуникации, но и получение "
+            "дополнительных функций для удобства жизни",
+            "name": "Смартфоны",
+            "products": [
+                {
+                    "description": "256GB, Серый цвет, 200MP камера",
+                    "name": "Samsung Galaxy C23 Ultra",
+                    "price": 180000.0,
+                    "quantity": 5,
+                },
+                {"description": "512GB, Gray space", "name": "Iphone 15", "price": 210000.0, "quantity": 8},
+                {"description": "1024GB, Синий", "name": "Xiaomi Redmi Note 11", "price": 31000.0, "quantity": 14},
+            ],
+        },
+        {
+            "description": "Современный телевизор, который позволяет наслаждаться просмотром, "
+            "станет вашим другом и помощником",
+            "name": "Телевизоры",
+            "products": [
+                {"description": "Фоновая подсветка", "name": '55" QLED 4K', "price": 123000.0, "quantity": 7}
+            ],
+        },
+    ]
 
 
 def test_make_from_json():
